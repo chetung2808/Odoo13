16024aac467de16d73b01ca68d6b65cdc55fc89e
chetung2808 <58966385+chetung2808@users.noreply.github.com>
2024-12-16 10:15:23 +0700

    Update tạo coupon
    Thêm Field 'header_code', 'length' , 'position' vào module 'sale.coupon.generate', sau đó thực hiện kế thừa view 'sale_coupon.sale_coupon_generate_view_form', chèn field mới vào.
    header_code : kí tự custom, ví dụ như 'NST'
    length: độ dài code
    position: vị trí cần chèn
    tổng độ dài counpon là : 7+ độ dài header_code.

---
+++ coupon_id/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'coupon custom',
+    'version': '13.0.0.0.1',
+    'category': 'Tutorials',
+    'summary': 'custom id CMT',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'CMT',
+    'maintainer': 'CMT',
+    'website': 'http://odoomates.tech',
+    'depends': ['sale_coupon'
+                ],
+    'demo': [],
+    'data': [
+         'views/coupon.xml',
+    ],
+    'installable': True,
+    'application': True,
+   
+}
